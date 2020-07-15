cea5a96cedfe68c7c0ffb28d90e0a2f38bafcce0
Stanislav Tibilius <tibilius@gmail.com>
2020-07-15 21:52:39 +0300

    lint(wishlist): lint

--- src/views/WishList.vue
+++ src/views/WishList.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
 
-        <a v-if="wishList.length" :href="
-        'data:application/csv;charset=utf-8,' + encodeURI($store.getters.getWishListDownload)
-        " download="wishlist.csv">
+        <a v-if="wishList.length"
+           :href="'data:application/csv;charset=utf-8,' + encodeURI($store.getters.getWishListDownload)"
+           download="wishlist.csv">
             Export to csv
         </a>
-    <DungItemList
-            v-bind:gItems="wishList"
-            v-bind:filteredClasses="filteredClasses"
+        <DungItemList
+                v-bind:gItems="wishList"
+                v-bind:filteredClasses="filteredClasses"
 
-    />
+        />
     </div>
 
 </template>
@@ -23,7 +23,7 @@
         components: {
             DungItemList
         },
-        computed:{
+        computed: {
             wishList() {
                 return this.$store.getters.getWishList;
             },
@@ -35,22 +35,22 @@
             '$route'(to) {
                 this.$store.dispatch('queryWishListUpdate', to.query)
             },
-            wishList:function () {
+            wishList: function () {
                 this.syncQueryParams(this.$route.query)
             }
         },
-        methods:{
-          syncQueryParams(toQuery){
-              let query = Object.assign({}, toQuery)
-              const wishListItems = JSON.stringify(this.$store.getters.getWishListItemsIds)
-              if (query.items !== wishListItems ) {
-                  this.wishList.length
-                      ? query.items = wishListItems
-                      : delete query.items
-                  this.$router.push({path: this.$route.path, query: query}).catch(() => {
-                  })
-              }
-          }
+        methods: {
+            syncQueryParams(toQuery) {
+                let query = Object.assign({}, toQuery)
+                const wishListItems = JSON.stringify(this.$store.getters.getWishListItemsIds)
+                if (query.items !== wishListItems) {
+                    this.wishList.length
+                        ? query.items = wishListItems
+                        : delete query.items
+                    this.$router.push({path: this.$route.path, query: query}).catch(() => {
+                    })
+                }
+            }
         },
         mounted() {
             this.$store.dispatch('queryWishListUpdate', this.$route.query)
