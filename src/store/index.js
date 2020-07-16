5cb080a9117cbbcc8c1638dca598053e2a312914
Stanislav Tibilius <tibilius@gmail.com>
2020-07-16 14:48:53 +0300

    refacor(vuex): change file name to human correct

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import dungenItems from "./modules/dungenItems";
+import dungeonItems from "./modules/dungeonItems";
 import wishlist from "./modules/wishlist";
 import common from "./modules/common";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    modules: {dungenItems, wishlist, common}
+    modules: {dungeonItems, wishlist, common}
 })
