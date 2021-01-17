0a84230fa7c765127ed55c004b076be151b038f3
Stanislav Tibilius <tibilius@gmail.com>
2021-01-17 09:00:59 +0300

    fix dungeons select

--- src/store/modules/dungeon.js
+++ src/store/modules/dungeon.js
@@ -1,14 +1,14 @@
 export default {
     state: {
         dungeon: [
-            {fullName: "Mists of Tirna Scithe", name: "MOTS", image: require('@/assets/wow_dungeons/shl/mots.webp')},
-            {fullName: "The Necrotic Wake", name: "NW", image: require('@/assets/wow_dungeons/shl/nw.webp')},
-            {fullName: "De Other Side", name: "DOS", image: require('@/assets/wow_dungeons/shl/dos.webp')},
-            {fullName: "Halls of Atonement", name: "HOA", image: require('@/assets/wow_dungeons/shl/hoa.webp')},
-            {fullName: "Plaguefall", name: "PF", image: require('@/assets/wow_dungeons/shl/pf.webp')},
-            {fullName: "Sanguine Depths", name: "SD", image: require('@/assets/wow_dungeons/shl/sd.webp')},
-            {fullName: "Spires of Ascension", name: "SOA", image: require('@/assets/wow_dungeons/shl/soa.webp')},
-            {fullName: "Theater of Pain", name: "TOP", image: require('@/assets/wow_dungeons/shl/top.webp')}
+            {fullName: "Mists of Tirna Scithe", name: "MOTS", checked:false, image: require('@/assets/wow_dungeons/shl/mots.webp')},
+            {fullName: "The Necrotic Wake", name: "NW", checked:false, image: require('@/assets/wow_dungeons/shl/nw.webp')},
+            {fullName: "De Other Side", name: "DOS", checked:false, image: require('@/assets/wow_dungeons/shl/dos.webp')},
+            {fullName: "Halls of Atonement", name: "HOA", checked:false, image: require('@/assets/wow_dungeons/shl/hoa.webp')},
+            {fullName: "Plaguefall", name: "PF", checked:false, image: require('@/assets/wow_dungeons/shl/pf.webp')},
+            {fullName: "Sanguine Depths", name: "SD", checked:false, image: require('@/assets/wow_dungeons/shl/sd.webp')},
+            {fullName: "Spires of Ascension", name: "SOA", checked:false, image: require('@/assets/wow_dungeons/shl/soa.webp')},
+            {fullName: "Theater of Pain", name: "TOP", checked:false, image: require('@/assets/wow_dungeons/shl/top.webp')}
         ],
     },
     mutations: {
